493f21a16aea74d188bb09cd107ddaa3909f5410
Ingo Ruhnke <grumbel@gmail.com>
2022-08-28 14:37:56 +0200

    Add --regex option

--- envpp/cmd_envpp.py
+++ envpp/cmd_envpp.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 
+import re
 import os
 import sys
 import argparse
@@ -12,21 +13,25 @@ def is_colon_path(text: str) -> bool:
     return text.find(':') != -1 and text.find('/') != -1
 
 
+def shall_include(args, name: str, value: str) -> bool:
+    return ((args.VARIABLE and name in args.VARIABLE) or
+            any([re.search(rx, name) for rx in args.regex]))
+
+
 def parse_args(argv: Sequence[str]) -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Pretty print environment variables")
-    parser.add_argument('VARIABLE', type=str, nargs='*', help='Variables to print')
+    parser.add_argument('VARIABLE', type=str, nargs='*', help='Print VARIABLE')
+    parser.add_argument('-r', '--regex', metavar='RX', type=str, action='append',
+                        help='Print variables matching regex')
     return parser.parse_args(argv[1:])
 
 
 def main(argv: Sequence[str]) -> None:
     args = parse_args(argv)
 
-    variables = list(os.environ.items())
-
-    if args.VARIABLE:
-        variables = [(name, value)
-                     for (name, value) in variables
-                     if name in args.VARIABLE ]
+    variables = [ (name, value)
+                  for (name, value) in os.environ.items()
+                  if shall_include(args, name, value) ]
 
     for name, value in variables:
         if is_colon_path(value):
